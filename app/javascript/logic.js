e0fb36cc5507074f353d8025d182a18ff3d5a8c8
SaumilBapat <sbapat@salesforce.com>
2018-12-21 13:09:50 -0500

    Updated post data

--- app/javascript/logic.js
+++ app/javascript/logic.js
@@ -9,7 +9,7 @@ function createQuestions() {
     question.append($('<h4>').html('Question ' + (i + 1)));
     question.append($('<p>').html(questions[i]));
     var options = createOptions();
-    options.attr('id', 'answer' + i);
+    options.attr('id', 'Answer' + i);
     question.append(options);
     question.append($('<br>'));
     question.append($('<br>'));
@@ -45,12 +45,12 @@ function submit() {
   event.preventDefault();
   var answers = [];
   for (var i = 0; i < questions.length; i++) {
-    let answerDiv = '#answer' + i;
+    let answerDiv = '#Answer' + i;
     let answer = $(answerDiv).val();
     answers.push(answer);
   }
   let postData = {
-    answers: answers,
+    answers: JSON.stringify(answers),
     name: $('#name').val(),
     profilePic: $('#profilePic').val()
   }
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 function initialize(app) {
   app.post('/api/friends', function(req, res) {
-      let name = req.body.name;
-      let profilePic = req.body.profilePic;
-      let answers = req.body.answers;
+      let body = req.body;
+      let name = body.name;
+      let profilePic = body.profilePic;
+      let answers = JSON.parse(body.answers);
       console.log(`~~~${name}`);
       console.log(`~~~${profilePic}`);
       console.log(`~~~${answers}`);
