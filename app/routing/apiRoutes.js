e99d2a5803d3b42c5ae9f922374e11683b3d74e2
SaumilBapat <sbapat@salesforce.com>
2018-12-21 12:32:50 -0500

    Update apiRoutes.js

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,12 @@
 const express = require('express');
 function initialize(app) {
   app.post('/api/friends', function(req, res) {
-      console.log(req.body);
+      let name = req.body.name;
+      let profilePic = req.body.profilePic;
+      let answers = req.body.answers;
+      console.log(`~~~${name}`);
+      console.log(`~~~${profilePic}`);
+      console.log(`~~~${answers}`);
       res.send('Test');
   });
 }
