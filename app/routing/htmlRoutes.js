b9a72a7d4983d05e2e39e309e205c2a3d04ee89e
SaumilBapat <sbapat@salesforce.com>
2018-12-21 12:17:27 -0500

    Created Procfile

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,34 +1,16 @@
-const express = require('express');
 const path = require('path');
-var bodyParser = require('body-parser')
-const PORT = process.env.PORT || 3000;
-const app = express();
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-
-// parse application/json
-app.use(bodyParser.json())
-
-app.get('/', function(req, res) {
-    var file = path.resolve(__dirname + '/../public/home.html');
-    console.log(file);
-    res.sendFile(file);
-});
-app.get('/survey', function(req, res) {
-    var file = path.resolve(__dirname + '/../public/survey.html');
-    console.log(file);
-    res.sendFile(file);
-});
-app.use(express.static('app'))
-app.post('/api/friends', function(req, res) {
-    console.log(req.body.name);
-    res.send('Test');
-});
-app.post('/api/data', (request, response) => {
-  const postBody = request.body;
-  console.log(postBody);
-  response.send('Test');
-});
-app.listen(PORT, function(status) {
-  console.log(`HTML Routes Listening on PORT ${PORT}`);
-});
+const express = require('express');
+function initialize(app) {
+  app.get('/', function(req, res) {
+      var file = path.resolve(__dirname + '/../public/home.html');
+      console.log(file);
+      res.sendFile(file);
+  });
+  app.get('/survey', function(req, res) {
+      var file = path.resolve(__dirname + '/../public/survey.html');
+      console.log(file);
+      res.sendFile(file);
+  });
+  app.use(express.static('app'));
+}
+module.exports = initialize;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const path = require('path');
+var bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3000;
 const app = express();
-require('./app/routing/htmlRoutes.js');
-//require('./app/routing/apiRoutes.js');
+app.use(bodyParser.urlencoded({ extended: false }))
+let htmlRoutes = require('./app/routing/htmlRoutes.js');
+htmlRoutes(app);
+let apiRoutes = require('./app/routing/apiRoutes.js');
+apiRoutes(app);
+
+app.listen(PORT, function(status) {
+  console.log(`HTML Routes Listening on PORT ${PORT}`);
+});
